7f7d8f77216030ec68aa49dd9f6cc99a8e0cf870
Ben Kannenberg <bkannenberg@gmail.com>
2017-07-05 15:00:25 +0930

    Add modern gulpfile

---
+++ gulpfile.js/tasks/htmlData.js
@@ -0,0 +1,27 @@
+var browserSync  = require('browser-sync')
+var config       = require('../config')
+var gulp         = require('gulp')
+var jsoncombine  = require("gulp-jsoncombine")
+var path         = require('path')
+var _            = require('lodash')
+
+var htmlDataTask = function(cb) {
+
+    return gulp.src(config.tasks.html.dataFiles)
+
+        .pipe(jsoncombine("global.json",function(data){
+
+            dataCC = _.mapKeys(data, function (v, k) {return _.camelCase(k);})
+
+            var sData = JSON.stringify(dataCC)
+
+            return new Buffer(sData)
+        }))
+        .pipe(gulp.dest("./src/html/data"))
+        .pipe(browserSync.stream())
+
+}
+
+gulp.task('htmlData', htmlDataTask)
+
+module.exports = htmlDataTask
