b36d440c8eea115dc6ebecdb366e32381873f741
Liam Rella <Liam.Rella@sa.gov.au>
2018-08-31 09:29:01 +0930

    2018 refresh initial commit

--- dist/js/prepare-ceec8acf5a.js
+++
@@ -1,1 +0,0 @@
-!function(){var e=document.querySelector("html");e.className=e.className.replace(/(?:^|\s)no-inlinesvg(?!\S)/,""),window.devicePixelRatio>1&&(e.className=e.className.replace(/(?:^|\s)no-retina(?!\S)/,""),e.className+=" retina")}();
